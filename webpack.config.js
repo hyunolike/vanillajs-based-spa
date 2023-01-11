e6cb2be10d6c34b4fb4cb859f0b4e9e6f8706a86
hh.jang <fallear1@gmail.com>
2023-01-11 23:38:30 +0900

    chore: 웹팩 설정 및 라이브러리 설치 (#3)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,37 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/main.js',
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 }
