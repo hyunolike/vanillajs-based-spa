5a72d0c15619c3d29761319e559b458d166eaa84
hh.jang <fallear1@gmail.com>
2023-01-18 23:18:35 +0900

    chore: 첫화면 로직 수정 (#6 #7)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './main.scss'
 import './reset.scss'
 
 import './views/home/home.scss'
+import  './views/detail/detail.scss'
 
 import {router} from './router/index.routes'
 
@@ -11,6 +12,11 @@ const init = () => {
     window.addEventListener("hashchange", () => {
         router(window.location.hash);
     })
+
+    // 페이지 자동 이동되게끔
+    if(document.location.href.substr(21) === '/'){
+        location.href = "#/";
+    }
 }
 
 window.addEventListener("load", init);
