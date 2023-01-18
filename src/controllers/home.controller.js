3eb9f4d60f2180d65dfa0e8e5ab3d9824e7ba205
hh.jang <fallear1@gmail.com>
2023-01-18 23:19:14 +0900

    feat: 메인 페이지 추가 및 수정 (#5 #6 #10)

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -10,12 +10,27 @@ export default () => {
 
     Axios.get(API_URL).then((res) => {
         let posts = res.data.data.posts;
-        console.log(posts);
+        // console.log(posts);
 
         posts.forEach((post) => {
-            let postEl = document.createElement('div')
-            postEl.innerHTML = post.title;
-            divElement.querySelector("#post-list-section").innerHTML += `<div>${post.title}</div>`
+            divElement.querySelector("#post-list-section").innerHTML += `
+             <div class="post" onClick="goDetail(${post.postId})">
+                <div class="post-main">
+                    <div class="post-img">
+                        <img src=${post.image}/ style="width:200px">
+                    </div>
+                    <div class="post-section">
+                        <div class="post-title">
+                            ${post.title}
+                        </div>
+                        <div class="post-detail">
+                            ${post.content}
+                        </div>  
+                    </div>  
+                </div>
+            </div>
+
+            `
         })
     });
 
