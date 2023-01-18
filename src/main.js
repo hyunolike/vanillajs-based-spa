1eb586bf64438d5652fa50f4667b2f960c3a6dd3
hh.jang <fallear1@gmail.com>
2023-01-18 11:10:12 +0900

    feat: UI 개발 세팅 (#5)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import './main.scss'
+import './reset.scss'
+
+import './views/home/home.scss'
+
 import {router} from './router/index.routes'
 
-window.addEventListener('hashchange', () => {
-    router(window.location.hash)
-})
+const init = () => {
+    router(window.location.hash);
 
+    window.addEventListener("hashchange", () => {
+        router(window.location.hash);
+    })
+}
+
+window.addEventListener("load", init);
 
 
